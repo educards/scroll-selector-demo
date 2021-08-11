20eaf3ecb6684ded0f036f40f6f641fe13343c17
Michal Vician <vicianm@gmail.com>
2021-08-11 17:33:18 +0200

    SelectionYSolver Bezier implementation

--- src/main/java/com/educards/scrollselectionviewdemo/SelectionYSolver.kt
+++ src/main/java/com/educards/scrollselectionviewdemo/SelectionYSolver.kt
@@ -1,11 +1,20 @@
 package com.educards.scrollselectionviewdemo
 
 import kotlin.math.absoluteValue
+import kotlin.math.pow
 import kotlin.math.sign
 
+/**
+ * @see curve
+ */
 class SelectionYSolver {
 
-    fun calculateSelectionYRatio(edgeDistanceTopPx: Int?, upwardsPerceptionRangePx: Int, edgeDistanceBottomPx: Int?, downwardsPerceptionRangePx: Int): Double {
+    fun calculateSelectionYRatio(
+        edgeDistanceTopPx: Int?,
+        upwardsPerceptionRangePx: Int,
+        edgeDistanceBottomPx: Int?,
+        downwardsPerceptionRangePx: Int
+    ): Double {
 
         if (edgeDistanceBottomPx == null
 
@@ -24,6 +33,61 @@ class SelectionYSolver {
             return distanceRatio * direction
         }
 
+    }
+
+    /**
+     * Computes a value of a function `f` with the following properties:
+     * * `f` is continuous
+     * * Is constant for `f(0) = 0`
+     * * Is constant for `f(width) = height`
+     * * `f` is monotonically decreasing (`x <= y | f(x) <= f(y)`)
+     *
+     * These properties ensure, that by proper combining of multiple curves the
+     * resulting curve will also have the same properties.
+     *
+     * Bezier curve is one possible implementation.
+     */
+    fun curve(
+        width: Double,
+        height: Double,
+        curvature: Double,
+        t: Double
+    ): Pair<Double, Double> {
+        return curveBezier(width, height, curvature, t)
+    }
+
+    private fun curveBezier(
+        width: Double,
+        height: Double,
+        curvature: Double,
+        t: Double
+    ): Pair<Double, Double> {
+
+        // B(t) = (1 - t)3P0 + 3(1-t)2tP1 + 3(1-t)t2P2 + t3P3
+
+        val x0 = .0
+        val y0 = .0
+
+        val x1 = curvature * width
+        val y1 = .0
+
+        val x2 = width - curvature * width
+        val y2 = height
+
+        val x3 = width
+        val y3 = height
+
+        val x = ((1 - t).pow(3) * x0) +
+                (3 * (1 - t).pow(2) * t * x1) +
+                (3 * (1 - t) * t.pow(2) * x2) +
+                (t.pow(3) * x3)
+
+        val y = ((1 - t).pow(3) * y0) +
+                (3 * (1 - t).pow(2) * t * y1) +
+                (3 * (1 - t) * t.pow(2) * y2) +
+                (t.pow(3) * y3)
+
+        return Pair(x, y)
     }
 
 }
