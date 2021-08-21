a04f9e257457b6133e6f4b7aa467b33a35bb8c08
Michal Vician <vicianm@gmail.com>
2021-08-21 12:27:32 +0200

    SelectionYDebugView.plotCurves fixed for the all cases

--- src/main/java/com/educards/scrollselectionviewdemo/SelectionYDebugView.kt
+++ src/main/java/com/educards/scrollselectionviewdemo/SelectionYDebugView.kt
@@ -1,12 +1,10 @@
 package com.educards.scrollselectionviewdemo
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
-import kotlin.math.absoluteValue
+import kotlin.math.*
 
 /**
  * Draws the trajectory and current state of `selectionY`.
@@ -18,7 +16,6 @@ class SelectionYDebugView: View {
     lateinit var selectionYParams: SelectionYParams
 
     private val paintSelectionRatio = Paint().apply {
-        isAntiAlias = true
         color = Color.BLUE
         strokeWidth = 60f
         alpha = 150
@@ -26,29 +23,38 @@ class SelectionYDebugView: View {
     }
 
     private val paintSelectionText = Paint().apply {
-        isAntiAlias = true
         color = Color.WHITE
-        textSize = 72f
+        textSize = 64f
     }
 
     private val paintScrollPxText = Paint().apply {
-        isAntiAlias = true
         color = Color.BLACK
-        textSize = 72f
+        textSize = 64f
     }
 
-    private val paintCurve = Paint().apply {
-        isAntiAlias = true
+    private val paintCurvePrimary = Paint().apply {
         color = Color.BLUE
-        strokeWidth = 3f
+        strokeWidth = 6f
         style = Paint.Style.STROKE
     }
 
-    private val paintCurveComposed = Paint().apply {
-        isAntiAlias = true
-        color = Color.BLACK
-        strokeWidth = 10f
+    private val paintCurveSecondary = Paint().apply {
+        color = Color.BLUE
+        strokeWidth = 1f
         style = Paint.Style.STROKE
+    }
+
+    private val paintPlotMid = Paint().apply {
+        color = Color.BLACK
+        strokeWidth = 1f
+        style = Paint.Style.STROKE
+        pathEffect = DashPathEffect(floatArrayOf(10f, 10f), 0f)
+    }
+
+    private val paintUnknownArea = Paint().apply {
+        color = Color.GRAY
+        alpha = 2
+        style = Paint.Style.FILL
     }
 
     @JvmOverloads
@@ -67,71 +73,157 @@ class SelectionYDebugView: View {
 
     private fun plotCurves(canvas: Canvas?) {
 
-        // Associations to 'val' just to prevent null checks
-        // and nullable ? types operators in rest of the code.
-        val contentTopDistPx = selectionYData?.contentTopDistPx
-        val contentBottomDistPx = selectionYData?.contentBottomDistPx
+        // Naming convention:
+        //   p* - plot domain
+        //   r* - real domain
 
-        if (contentTopDistPx != null && SelectionYData.isContentTopDetected(contentTopDistPx)
-           && contentBottomDistPx != null && SelectionYData.isContentBottomDetected(contentBottomDistPx)) {
+        val rTopDist = selectionYData.contentTopDistPx?.absoluteValue
+        val rTopPerceptRange = selectionYParams.contentTopPerceptionRangePx
+        val rBottomDist = selectionYData.contentBottomDistPx
+        val rBottomPerceptRange = selectionYParams.contentBottomPerceptionRangePx
+        val rTotalPerceptRange = rTopPerceptRange + rBottomPerceptRange
+        val r2pCoef = width.toFloat() / rTotalPerceptRange.toFloat()
 
-            val contentHeightPx = (contentTopDistPx.absoluteValue + contentBottomDistPx.absoluteValue).toFloat()
-            val content2PlotCoef = width.toFloat() / contentHeightPx
+        val pTopPerceptRange = rTopPerceptRange * r2pCoef
+        canvas?.drawLine(pTopPerceptRange, 0f, pTopPerceptRange, height.toFloat(), paintPlotMid)
 
-            for (i in 0..width) {
+        for (i in 0..width) {
 
-                val topCurveHeightScaled = selectionYParams.selectionYMid * height
-                val bottomCurveHeightScaled = (1 - selectionYParams.selectionYMid) * height
+            val rX = (i.toDouble() / r2pCoef).roundToInt()
 
-                val topPerceptionRangeScaled = selectionYParams.contentTopPerceptionRangePx.toDouble() * content2PlotCoef
-                val pTop = if (i > topPerceptionRangeScaled) Pair(i.toDouble(), topCurveHeightScaled) else {
-                    selectionYSolver.curve(
-                        topPerceptionRangeScaled,
-                        topCurveHeightScaled,
-                        1 - selectionYParams.stiffness,
-                        i.toDouble()
-                    )
+            if (rTopDist != null && rBottomDist != null) {
+
+                val rTotalDist = (rTopDist + rBottomDist).toDouble()
+                val rTopBottomPerceptRatio = rTopPerceptRange.toDouble() / rTotalPerceptRange.toDouble()
+                val rTopStart = rTopPerceptRange - (rTotalDist * rTopBottomPerceptRatio)
+                val rTopY = when {
+                    rX < rTopStart -> {
+                        0.0
+                    }
+                    rX > rTopStart + rTopPerceptRange -> {
+                        selectionYParams.selectionYMid
+                    }
+                    else -> {
+                        val rTopX = rX - rTopStart
+                        selectionYSolver.curve(
+                            rTopPerceptRange.toDouble(),
+                            selectionYParams.selectionYMid,
+                            1.0 - selectionYParams.stiffness,
+                            rTopX)?.second
+                    }
+                }
+                if (rTopY != null) canvas?.drawPoint(i.toFloat(), rTopY.toFloat() * height, paintCurveSecondary)
+
+                val rBottomStart = rTopPerceptRange - rBottomPerceptRange + rTotalDist * (1.0 - rTopBottomPerceptRatio)
+                val rBottomY = when {
+                    rX < rBottomStart -> {
+                        0.0
+                    }
+                    rX > rBottomStart + rBottomPerceptRange -> {
+                        1.0 - selectionYParams.selectionYMid
+                    }
+                    else -> {
+                        val rBottomX = rX - rBottomStart
+                        selectionYSolver.curve(
+                            rBottomPerceptRange.toDouble(),
+                            1.0 - selectionYParams.selectionYMid,
+                            1.0 - selectionYParams.stiffness,
+                            rBottomX)?.second
+                    }
+                }
+                if (rBottomY != null) {
+                    val rBottomYPlotted = rBottomY + selectionYParams.selectionYMid
+                    canvas?.drawPoint(i.toFloat(), rBottomYPlotted.toFloat() * height, paintCurveSecondary)
                 }
 
-                val bottomPerceptionRangeScaled = selectionYParams.contentBottomPerceptionRangePx.toDouble() * content2PlotCoef
-                val bottomShift = width - bottomPerceptionRangeScaled
-                val pBottom = if (i < bottomShift) Pair(i.toDouble(), 0.0) else {
+                val rComposedY = if (rTopY != null && rBottomY != null) {
+                    val rWeightFrom = max(rTopStart, rBottomStart)
+                    val rWeightTo = min (rTopStart + rTopPerceptRange, rBottomStart + rBottomPerceptRange)
+                    val rWeightDist = rWeightTo - rWeightFrom
+                    var topWeight = if (rX < rWeightFrom) 1.0 else if (rX > rWeightTo) 0.0 else 1 - ((rX - rWeightFrom) / rWeightDist)
+                    topWeight = sqrt(topWeight)
+                    var bottomWeight = if (rX < rWeightFrom) 0.0 else if (rX > rWeightTo) 1.0 else (rX - rWeightFrom) / rWeightDist
+                    bottomWeight = sqrt(bottomWeight)
+                    val rTopYCentered = rTopY - selectionYParams.selectionYMid
+                    (rTopYCentered * topWeight + rBottomY * bottomWeight) + selectionYParams.selectionYMid
+                } else null
+                if (rComposedY != null) canvas?.drawPoint(i.toFloat(), rComposedY.toFloat() * height, paintCurvePrimary)
 
-                    val p = selectionYSolver.curve(
-                        bottomPerceptionRangeScaled,
-                        bottomCurveHeightScaled,
-                        1 - selectionYParams.stiffness,
-                        i.toDouble() - bottomShift
-                    )
+                canvas?.drawRect(0f, 0f, (rTopStart * r2pCoef).toFloat(), bottom.toFloat() ,paintUnknownArea)
+                canvas?.drawRect(((rTopStart + rTotalDist) * r2pCoef).toFloat(), 0f, width.toFloat(), bottom.toFloat() ,paintUnknownArea)
 
-                    if (p == null) null else Pair(
-                        bottomShift + p.first,
-                        p.second
-                    )
+            } else if (rTopDist != null) {
+
+                val rTopStart = 0
+                val rTopY = when {
+                    rX < rTopStart -> {
+                        0.0
+                    }
+                    rX > rTopStart + rTopPerceptRange -> {
+                        selectionYParams.selectionYMid
+                    }
+                    else -> {
+                        val rTopX = (rX - rTopStart).toDouble()
+                        selectionYSolver.curve(
+                            rTopPerceptRange.toDouble(),
+                            selectionYParams.selectionYMid,
+                            1 - selectionYParams.stiffness,
+                            rTopX)?.second
+                    }
                 }
+                canvas?.drawRect(rTopPerceptRange * r2pCoef, 0f, width.toFloat(), bottom.toFloat() ,paintUnknownArea)
+                if (rTopY != null) canvas?.drawPoint(i.toFloat(), rTopY.toFloat() * height, paintCurvePrimary)
 
-                if (pTop != null) canvas?.drawPoint(i.toFloat(), pTop.second.toFloat(), paintCurve)
-                if (pBottom != null) canvas?.drawPoint(i.toFloat(), pBottom.second.toFloat() + topCurveHeightScaled.toFloat(), paintCurve)
+            } else if (rBottomDist != null) {
 
-                if (pTop != null && pBottom != null) {
-                    val pComposed = Pair(
-                        i.toDouble(),
-                        pTop.second + pBottom.second
-                    )
-                    canvas?.drawPoint(i.toFloat(), pComposed.second.toFloat(), paintCurveComposed)
+                val rBottomStart = rTopPerceptRange
+                val rBottomY = when {
+                    rX < rBottomStart -> {
+                        selectionYParams.selectionYMid
+                    }
+                    rX > rBottomStart + rBottomPerceptRange -> {
+                        1.0
+                    }
+                    else -> {
+                        val rBottomX = (rX - rBottomStart).toDouble()
+                        val rBottomY = selectionYSolver.curve(
+                            rBottomPerceptRange.toDouble(),
+                            1.0 - selectionYParams.selectionYMid,
+                            1 - selectionYParams.stiffness,
+                            rBottomX)?.second
+                        rBottomY?.plus(selectionYParams.selectionYMid)
+                    }
                 }
+                canvas?.drawRect(0f, 0f, rBottomStart * r2pCoef, bottom.toFloat() ,paintUnknownArea)
+                if (rBottomY != null) canvas?.drawPoint(i.toFloat(), rBottomY.toFloat() * height, paintCurvePrimary)
+
+            } else {
+
+                val rMidY = selectionYParams.selectionYMid
+                canvas?.drawRect(0f, 0f, width.toFloat(), height.toFloat(), paintUnknownArea)
+                if (rMidY != null) canvas?.drawPoint(i.toFloat(), rMidY.toFloat() * height, paintCurvePrimary)
             }
         }
     }
 
     private fun plotSelectionAndDistTexts(canvas: Canvas?) {
+
         selectionYData?.let { data ->
             data.selectionY?.let { selection ->
                 val selectionYPx = (height * selection).toFloat()
+
+                data.contentTopDistPx.let { topDist ->
+                    val distText = if (topDist == null) "beyond perception" else "$topDist px"
+                    canvas?.drawText("top distance: $distText", 72f, selectionYPx - 48f, paintScrollPxText)
+                }
+
                 canvas?.drawLine(0f, selectionYPx, width.toFloat(), selectionYPx, paintSelectionRatio)
                 canvas?.drawText("selection: ${data.selectionY}", 72f, selectionYPx + 24f, paintSelectionText)
-                canvas?.drawText("content top distance: ${data.contentTopDistPx} px", 72f, selectionYPx - 48f, paintScrollPxText)
-                canvas?.drawText("content bottom distance: ${data.contentBottomDistPx} px", 72f, selectionYPx + 92f, paintScrollPxText)
+
+                data.contentBottomDistPx.let { bottomDist ->
+                    val distText = if (bottomDist == null) "beyond perception" else "$bottomDist px"
+                    canvas?.drawText("bottom distance: $distText", 72f, selectionYPx + 92f, paintScrollPxText)
+                }
             }
         }
     }
--- src/main/java/com/educards/scrollselectionviewdemo/SelectionYParams.kt
+++ src/main/java/com/educards/scrollselectionviewdemo/SelectionYParams.kt
@@ -5,8 +5,21 @@ package com.educards.scrollselectionviewdemo
  */
 data class SelectionYParams(
 
+    /**
+     * Positive integer which determines how far will the algorithm
+     * look for finding the top edge of the content wrapped in [RecyclerView].
+     * * **Top edge distance value**: The top edge distance is null (not detected withing the range)
+     * or a positive integer (detected) which is always <= as this value (`contentTopDistancePx <= contentTopPerceptionRangePx`).
+     * * **Impact on transition curve**: The bigger the `contentTopPerceptionRangePx` the smoother is the transition from
+     * `selectionYMid` to content edge.
+     * * **Impact on performance**: Big values of `contentTopPerceptionRangePx` may result in poor performance, because
+     * the algorithm internally creates `View` instance to measure the dimension of hidden elements.
+     */
     var contentTopPerceptionRangePx: Int = 2500,
 
+    /**
+     * Positive integer which is the equivalent of [contentTopPerceptionRangePx] for detecting the content bottom edge.
+     */
     var contentBottomPerceptionRangePx: Int = 2500,
 
     /**
