53368e2dd86155f5810ba820ec849e9d644a591c
Michal Vician <vicianm@gmail.com>
2021-09-08 21:03:52 +0200

    Documentation

--- src/main/java/com/educards/scrollselector/demo/SelectionData.kt
+++ src/main/java/com/educards/scrollselector/demo/SelectionData.kt
@@ -8,9 +8,9 @@ data class SelectionData(
 
     /**
      * The `selectionRatio` of the scrollable [View]'s viewport:
-     * * 0.0 - the top most edge of the [View]
+     * * 0.0 - the top edge of the [View]
      * * 0.5 - middle part of the [View]
-     * * 1.0 - the bottom most edge of the [View]
+     * * 1.0 - the bottom edge of the [View]
      */
     var selectionRatio: Double? = null,
 
